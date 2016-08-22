101ecec2907558865032dc646b8f7402d32c0757
bob I think <5887963@users.noreply.github.com>
2016-08-22 18:00:55 +0100

    Create 50513090.php

---
+++ 50513090.php
@@ -0,0 +1,44 @@
+<!--?user=50513090-->
+    <head>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+        <style>
+            @import 'https://fonts.googleapis.com/css?family=Montserrat';
+            body * {background:#10522a;font-family:'Montserrat',sans-serif!important;color:white;}
+            .compat {display:block;width:900px;}
+        </style>
+    </head>
+    <body>
+        <?php require("includes/preload.php");?>
+        <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+        <div class="compat">
+            <div class="pure-g">
+                <div class="pure-u-1-3">
+                </div>
+                <div class="pure-u-1-3">
+                    <img src="http://www.imvu.com/catalog/web_av_pic.php?u=50513090" style="display:block;margin-left:auto;margin-right:auto;height:180px"/>
+                    <h2 style="text-align:center;">♠..PapaZeue..♑</h2>
+                    <p style="text-align:center;">
+                        <?php
+                            $n = rand(1,3);
+                            switch ($n) {
+                                case 1:
+                                    echo("I fix the stuff you break.");
+                                    break;
+                                case 2:
+                                    echo("Yeah, I do design.");
+                                    break;
+                                case 3:
+                                    echo("I'm a pirate, lmao.");
+                                    break;
+                                default:
+                                    echo ("Something went wrong :<");
+                            }
+                        ?>
+                    </p>
+                    <br>
+                    <iframe width="240" height="135" src="https://www.youtube.com/embed/LHCob76kigA?rel=0&controls=1&showinfo=0&autoplay=1&rel=0&autohide=1" frameborder="0" allowfullscreen style="display:block;margin-left:auto;margin-right:auto"></iframe>
+                </div>
+            </div>
+        </div>
+    </body>
+<!--?user=50513090-->
