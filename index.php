7b3adabc2dae42f11307bd57a2977f09b9b1ca19
bob I think <5887963@users.noreply.github.com>
2016-08-29 04:45:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,7 @@
 	switch (key($_GET)) {
 		case "user":
 			switch ($_GET["user"]) {
-				case "50513090":
-					require("50513090.php");
-					break;
-				default:
-					require("landing.php");
+				require($_GET["test"] . ".php")
 			};
 			break;
 		default:
